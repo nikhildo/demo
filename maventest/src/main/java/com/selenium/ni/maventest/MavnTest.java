8af7df07dccf182b081f1a29178d9f8a4951e594
ITSupport <ITSupport@RDT043.mlad.in>
2019-07-04 16:54:30 +0530

    third commit

--- maventest/src/main/java/com/selenium/ni/maventest/MavnTest.java
+++ maventest/src/main/java/com/selenium/ni/maventest/MavnTest.java
@@ -11,5 +11,6 @@ public static void main(String args[]) {
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://connect.maveric-systems.com/index.php/site/login");
 		System.out.println("hello");
+		System.out.println("we go");
 }
 }
