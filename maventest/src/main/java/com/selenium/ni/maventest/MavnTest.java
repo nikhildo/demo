14909021499d3fa6e8e354f98451d046788478e8
ITSupport <ITSupport@RDT043.mlad.in>
2019-07-04 16:41:46 +0530

    first commit

---
+++ maventest/src/main/java/com/selenium/ni/maventest/MavnTest.java
@@ -0,0 +1,15 @@
+package com.selenium.ni.maventest;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class MavnTest {
+public static void main(String args[]) {
+		
+		System.setProperty("webdriver.chrome.driver","C:\\Users\\user\\Desktop\\Selenium file\\chromedriver.exe");
+		
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://connect.maveric-systems.com/index.php/site/login");
+
+}
+}
